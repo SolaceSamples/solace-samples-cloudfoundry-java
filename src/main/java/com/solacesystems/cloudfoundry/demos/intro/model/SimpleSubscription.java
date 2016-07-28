e123f05f965434259772f605b5af9bd04f3ff140
Francois Dickey <francois.dickey@solacesystems.com>
2016-07-28 15:31:38 -0400

    Initial commit of the Solace for Cloud Foundry Java Sample Application

---
+++ src/main/java/com/solacesystems/cloudfoundry/demos/intro/model/SimpleSubscription.java
@@ -0,0 +1,17 @@
+package com.solacesystems.cloudfoundry.demos.intro.model;
+
+public class SimpleSubscription {
+	private String subscription;
+
+	public SimpleSubscription() {
+		subscription = "";
+	}
+
+	public String getSubscription() {
+		return subscription;
+	}
+
+	public void setSubscription(String subscription) {
+		this.subscription = subscription;
+	}
+}
